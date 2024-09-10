ff414adff9334122ae80c06b156b3fe889a02997
Montano Kiseo <tanexmj@gmail.com>
2024-09-10 23:07:14 +0800

    Yes Sir

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -12,24 +12,23 @@
 
 
         {{-- Content --}}
-        <div class="w-3/5 bg-green-500 text-white p-2">
-            <div class="flex flex-col w-full bg-white p-4 rounded-lg shadow-md space-y-3 mb-4 text-black">
-                <div class="flex items-start space-x-4">
+        <div class="w-3/5 text-white p-2">
+            <div class="flex flex-col w-full bg-white p-8 rounded-lg shadow-md space-y-3 mb-4 text-black">
+                <div class="flex items-start space-x-3">
                     <img src="https://img.icons8.com/?size=100&id=fUUEbUbXhzOA&format=png&color=000000" class="w-10 h-10 rounded-full" alt="Icon">
-                    <div class="flex-1">
-                        <figure class="w-full h-64 overflow-hidden rounded-lg">
+                    <div class="flex-1 pr-4">
+                        <div class="flex items-center mb-2 space-x-2">
+                            <div class="font-bold">@TanexDaddy</div>
+                            <div class="text-gray-500 text-sm">Created 2 hours ago</div>
+                        </div>
+                        <div class="w-full h-80 overflow-hidden rounded-lg">
                             <img class="w-full h-full object-cover" src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes">
-                        </figure>
-                        <div class="card-body">
-                            <h2 class="card-title text-black">
-                                Shoes!
-                                <div class="badge badge-secondary">NEW</div>
-                            </h2>
-                            <p class="text-black">If a dog chews shoes whose shoes does he choose?</p>
-                            <div class="card-actions justify-end">
-                                <div class="badge badge-outline">Fashion</div>
-                                <div class="badge badge-outline">Products</div>
-                            </div>
+                        </div>
+                        <div class="mt-4 w-full">
+                            <h1 class="text-3xl font-bold mb-2">SHOES</h1>
+                            <p class="text-black text-start text-gray-500">
+                                Step up your style game with our latest shoe collection! Whether you’re hitting the gym, the streets, or just lounging at home, we’ve got the perfect pair for every occasion. From sleek sneakers to cozy slip-ons, find your new favorite footwear and walk with confidence. 🏃‍♂️👟 #Shoes #Fashion #Style
+                            </p>
                         </div>
                     </div>
                 </div>
